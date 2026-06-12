a1b56b07f7882742417af576fb0da5b5537057c9
Debbido27 <davidmedina272004@gmail.com>
2026-06-12 15:41:05 -0600

    Paleta de colores creada

--- Sistema Bancario/src/GUI/LoginFrame.java
+++ Sistema Bancario/src/GUI/LoginFrame.java
@@ -1,4 +1,17 @@
 package GUI;
 
+import java.awt.*;
+
 public class LoginFrame {
+
+    //PALETA DE COLORES
+    private static final Color ROJO_BA      = new Color(180, 20, 20);
+    private static final Color ROJO_BOTON   = new Color(160, 15, 15);
+    private static final Color ROJO_HOVER   = new Color(200, 30, 30);
+    private static final Color GRIS_CAMPO   = new Color(245, 245, 245);
+    private static final Color GRIS_TEXTO   = new Color(120, 120, 120);
+    private static final Color BLANCO       = Color.WHITE;
+
+    
+
 }
